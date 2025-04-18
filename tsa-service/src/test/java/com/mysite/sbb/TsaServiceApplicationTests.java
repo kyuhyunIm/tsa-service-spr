6bc0e85e911083b916a003ce82fb823f9306a2ca
정지원 <jidole041214@naver.com>
2025-04-18 10:41:20 +0900

    init

---
+++ tsa-service/src/test/java/com/mysite/sbb/TsaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
