6cb17bd002efce7a04519a92559bdfbb16fdc048
정지원 <jidole041214@naver.com>
2025-04-21 15:26:59 +0900

    wip

--- tsa-service/src/main/java/com/goono/tsaService/TsaServiceController.java
+++ tsa-service/src/main/java/com/goono/tsaService/TsaServiceController.java
@@ -9,25 +9,9 @@ import com.goono.tsaService.tsaCertificate.TsaRunner;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 public class TsaServiceController {
-	private final TsaRunner tsaRunner;
-	
 	@GetMapping("/")
 	public String root() {
-		return "tsa_certificate_list";
-	}
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		try {
-			tsaRunner.ready();
-			String result = tsaRunner.run("test_hash");
-			return result;
-		} catch(Exception e){
-			e.printStackTrace();
-			return "error";
-		}
+		return "tsa_certificate_form";
 	}
 }
