d1c8f82c0bfbb9a18372503bd072a83d48db50a3
정지원 <jidole041214@naver.com>
2025-04-21 10:27:50 +0900

    fix TsaCertificate

--- tsa-service/src/main/java/com/goono/tsaService/tsaCertificate/TsaCertificate.java
+++ tsa-service/src/main/java/com/goono/tsaService/tsaCertificate/TsaCertificate.java
@@ -1,5 +1,6 @@
 package com.goono.tsaService.tsaCertificate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
@@ -10,17 +11,24 @@ import lombok.Setter;
 @Entity
 public class TsaCertificate {
 	@Id
+	@Column(nullable = false)
 	private String hash;
 	
+	@Column(nullable = false)
 	private String policy;
 	
+	@Column(nullable = false)
 	private String algorithm;
 	
+	@Column(nullable = false)
 	private String serialNumber;
 	
+	@Column(nullable = false)
 	private String issuer;
 	
+	@Column(nullable = false)
 	private String nonce;
 	
+	@Column(nullable = false)
 	private String timestamp;
 }
