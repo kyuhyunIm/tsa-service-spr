db51335ce2a476cefb73c865ef2d75620ca95395
정지원 <jidole041214@naver.com>
2025-04-21 16:04:08 +0900

    add getList method

--- tsa-service/src/main/java/com/goono/tsaService/tsaCertificate/TsaCertificateService.java
+++ tsa-service/src/main/java/com/goono/tsaService/tsaCertificate/TsaCertificateService.java
@@ -1,5 +1,7 @@
 package com.goono.tsaService.tsaCertificate;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
@@ -15,5 +17,9 @@ public class TsaCertificateService {
 		String tsaToken = tsaRunner.run(hash);
 		TsaCertificate result = tsaTokenParser.parse(tsaToken);
 		tsaCertificateRepository.save(result);
+	}
+	
+	public List<TsaCertificate> getList() {
+		return tsaCertificateRepository.findAll();
 	}
 }
